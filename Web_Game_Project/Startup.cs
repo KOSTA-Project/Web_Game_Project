e501d28638f9a5e858463029a94b020807fa5ca3
ParkJooHyeong <67997760+ParkJooHyeong@users.noreply.github.com>
2021-05-07 01:41:36 +0900

    SignalR 통신 실험.

--- Web_Game_Project/Startup.cs
+++ Web_Game_Project/Startup.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace Web_Game_Project
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+namespace SignalRChat
 {
     public class Startup
     {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
     }
 }
